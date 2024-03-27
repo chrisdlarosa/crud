4ec7ffdfd5222c15b2a731bfcde30c5e90216edc
Chris dlrosa <chris.dlarosac@gmail.com>
2024-03-26 22:10:09 -0600

    Se agrego proyecto

---
+++ WebApp/Controllers/CustomersController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        // GET: CustomersController
+
+        public CustomersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Index()
+        {
+            IEnumerable<Customers> listaCustomers = _context.customers;
+            return View(listaCustomers);
+        }
+
+        // GET: CustomersController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CustomersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Customers customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.customers.Add(customer);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // GET: CustomersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var cust = _context.customers.Find(id);
+            return View(cust);
+        }
+
+        // POST: CustomersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Customers customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.customers.Update(customer);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // GET: CustomersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApp/Models/Products.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Products
+    {
+        [Key]
+        public int PRODUCT_ID { get; set; }
+        public string? PRODUCT_NAME { get; set; }
+        public string? DESCRIPTION { get; set; }
+        public decimal STANDARD_COST { get; set; }
+        public decimal LIST_PRICE { get; set; }
+        [ForeignKey("Product_Categories")]
+        public int CATEGORY_ID { get; set; }
+    }
+}
